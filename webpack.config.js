77f17f2d3a5f40481e39cc2f7a69181700967d66
krokowski0x <krokowski.dx@gmail.com>
2018-05-04 00:03:20 +0200

    Initial webpack + babel setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.worker\.js$/,
+        use: { loader: 'worker-loader' }
+      },
+      {
+        test: /\.scss$/,
+        use: [ {
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        } ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
