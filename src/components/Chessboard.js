2d78392f85111400e1eaaa065553e223d293f4ff
krokowski0x <krokowski.dx@gmail.com>
2018-05-09 19:26:12 +0200

    Rendered pieces to board

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const boardArray = Array.from({length: 64}, (value, key) => key);
-const calcX = index => (index % 8) * 10;
-const calcY = index => Math.floor(index / 8) * 10;
+const calcX = index => (index % 8) * 12.5;
+const calcY = index => Math.floor(index / 8) * 12.5;
 
-const Chessboard = () => {
+const Chessboard = (props) => {
+  const boardAscii = props.game.ascii().replace(/[\d+\-\| \n]/gi, '').replace(/\./g,'a').replace(/abcdefgh/,'').split('');
   return (
     <div className='chessboard'>
-      <svg viewBox="0 0 220 100" >
-        <rect x="0" y="0" width="80" height="80" />
-        {boardArray.map((square, index) => {
-          return <rect
-            key={index}
-            x={calcX(index)}
-            y={calcY(index)}
-            width="10"
-            height="10"
-            class={
-              calcY(index) % 20
-              ? (!(index % 2) ? 'black' : 'white')
-              : ((index % 2) ? 'black' : 'white')
-            }
-          />;
+      <svg width="640" height="640" viewBox="0 0 100 100">
+        {boardAscii.map((square, index) => {
+          return (
+            <g key={index}>
+              <rect
+              x={calcX(index)}
+              y={calcY(index)}
+              width="12.5"
+              height="12.5"
+              className={
+                calcY(index) % 25
+                ? (!(index % 2) ? 'black' : 'white')
+                : ((index % 2) ? 'black' : 'white')
+              }
+            />
+            </g>);
         })}
       </svg>
+      {boardAscii.map((piece, index) => {
+        if (piece.toLowerCase() === piece)
+          piece += 'b';
+        else
+          piece +='w';
+
+        return (
+          <img
+            key={index}
+            src={`src/pieces/${piece}.svg`}
+            height="80px"
+            width="80px"
+          />);
+      })}
     </div>
   );
 }
