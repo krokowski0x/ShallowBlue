1278491fd9aaaef63f559cb4b50bca03420ce011
krokowski0x <krokowski.dx@gmail.com>
2018-05-09 16:28:04 +0200

    Added MovesHistory component

---
+++ src/components/MovesHistory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const MovesHistory = () => {
+  return (
+    <div className="moves">
+      <h2>Moves</h2>
+      <hr />
+    </div>
+  );
+}
+
+export default MovesHistory;
