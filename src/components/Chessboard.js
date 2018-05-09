245f7da74713a68559308fce2bfd4e261cc5e970
krokowski0x <krokowski.dx@gmail.com>
2018-05-09 20:48:58 +0200

    Added basic drag n' drop handler

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Draggable from 'react-draggable';
 import PropTypes from 'prop-types';
 
 const calcX = index => (index % 8) * 12.5;
 const calcY = index => Math.floor(index / 8) * 12.5;
 
 const Chessboard = (props) => {
-  const boardAscii = props.game.ascii().replace(/[\d+\-\| \n]/gi, '').replace(/\./g,'a').replace(/abcdefgh/,'').split('');
+  const boardAscii = props.game.ascii()
+    .replace(/[\d+\-\| \n]/gi, '')
+    .replace(/\./g,'a')
+    .replace(/abcdefgh/,'')
+    .split('');
+console.log(props.game.ascii());
   return (
     <div className='chessboard'>
       <svg width="640" height="640" viewBox="0 0 100 100">
@@ -33,12 +39,17 @@ const Chessboard = (props) => {
           piece +='w';
 
         return (
-          <img
+          <Draggable
             key={index}
-            src={`src/pieces/${piece}.svg`}
-            height="80px"
-            width="80px"
-          />);
+            //bounds="parent"
+            disabled={piece === 'ab' ? true : false}
+            grid={[80, 80]}>
+            <img
+              src={`src/pieces/${piece}.svg`}
+              height="80px"
+              width="80px"
+            />
+          </Draggable>);
       })}
     </div>
   );
