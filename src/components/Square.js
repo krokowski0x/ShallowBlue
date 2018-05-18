b37199d7b4129ab2bb618231c649d52f7790acb0
krokowski0x <krokowski.dx@gmail.com>
2018-05-18 20:40:00 +0200

    Added separate Square component

---
+++ src/components/Square.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const calcX = index => (index % 8) * 12.5;
+const calcY = index => Math.floor(index / 8) * 12.5;
+
+const Square = (props) => {
+  const { index } = props;
+
+  return (
+    <rect
+      onClick={() => console.log(index)}
+      key={`${String.fromCharCode(Math.floor(index / 8) + 97)}${(index % 8) + 1}`}
+      x={calcX(index)}
+      y={calcY(index)}
+      width="12.5"
+      height="12.5"
+      className={
+        calcY(index) % 25
+        ? (!(index % 2) ? 'black' : 'white')
+        : ((index % 2) ? 'black' : 'white')
+      }
+    />
+  );
+};
+
+export default Square;
