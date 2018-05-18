9cd211b382e275245a8687cf9d0f2606e2b26e3b
krokowski0x <krokowski.dx@gmail.com>
2018-05-18 22:56:51 +0200

    Finally made Draggable Pieces work as expected

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import Draggable from 'react-draggable';
 
+import Square from './Square';
+
 class Chessboard extends Component {
   constructor(props) {
     super(props);
     this.calcX = index => (index % 8) * 12.5;
     this.calcY = index => Math.floor(index / 8) * 12.5;
+    this.handleStart = (e) => {
+      const square = `${String.fromCharCode(Math.floor(e.clientX / 80) + 97)}${9-Math.floor(e.clientY / 80)}`;
+      console.log(square);
+      this.setState({ from: square });
+    };
+    this.handleStop = (e) => {
+      const square = `${String.fromCharCode(Math.floor(e.x / 80) + 97)}${9-Math.floor(e.y / 80)}`;
+      this.state.chess.move({ from: this.state.from, to: square });
+      this.setState(this.state.chess);
+    };
     this.state = {
       chess: props.chess,
+      boardAscii: props.chess.ascii()
+        .replace(/[\d+\-|\s]/gi, '')
+        .replace(/\./g, 'a')
+        .replace(/abcdefgh/, '')
+        .split(''),
     };
-    this.boardAscii = this.state.chess.ascii()
-      .replace(/[\d+\-|\s]/gi, '')
-      .replace(/\./g, 'a')
-      .replace(/abcdefgh/, '')
-      .split('');
   }
 
   render() {
@@ -21,21 +33,9 @@ class Chessboard extends Component {
     return (
       <div className="chessboard">
         <svg width="640" height="640" viewBox="0 0 100 100">
-          {this.boardAscii.map((square, index) => (
-            <rect
-              key={`${String.fromCharCode(Math.floor(index / 8) + 97)}${(index % 8) + 1}`}
-              x={this.calcX(index)}
-              y={this.calcY(index)}
-              width="12.5"
-              height="12.5"
-              className={
-                this.calcY(index) % 25
-                ? (!(index % 2) ? 'black' : 'white')
-                : ((index % 2) ? 'black' : 'white')
-              }
-            />))}
+          {this.state.boardAscii.map((square, index) => <Square key={index} index={index} />)}
         </svg>
-        {this.boardAscii.map((piece, index) => {
+        {this.state.boardAscii.map((piece, index) => {
           if (piece.toLowerCase() === piece)
             piece += 'b';
           else
@@ -43,14 +43,16 @@ class Chessboard extends Component {
 
           return (
             <Draggable
-              key={index}
+              key={piece + index}
               // bounds="parent"
               disabled={piece === 'ab'}
-              onStop={e => this.props.chess.move(`${String.fromCharCode(Math.floor(e.x / 80) + 97)}${Math.floor((e.y - 37) / 80)}`)}
+              onStart={this.handleStart}
+              onStop={this.handleStop}
               grid={[80, 80]}
             >
               <img
                 alt="Chess Piece"
+                draggable="false"
                 src={`src/pieces/${piece}.svg`}
                 height="80px"
                 width="80px"
