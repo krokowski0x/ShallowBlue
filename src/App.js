a60bbc780735c455f9a291877291bee87efe33ac
krokowski0x <krokowski.dx@gmail.com>
2018-05-19 14:46:21 +0200

    Added rows and cols naming

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Chessboard from './components/Chessboard';
 import MovesHistory from './components/MovesHistory';
 
 const chess = new Chess();
+const helperArray = Array.from({ length: 8 }, (v, k) => k + 1);
 
 const App = () => (
   <div className="App">
     <h1>ShallowBlue.js</h1>
+    <div className="rows">{helperArray.map(i => <p key={i}>{i}</p>)}</div>
     <Chessboard chess={chess} />
     <MovesHistory />
+    <div className="cols">{helperArray.map(i => <p key={i}>{String.fromCharCode(i + 96)}</p>)}</div>
   </div>
 );
 
