3e7e873929c766beaed337a1f72b039f62524162
krokowski0x <krokowski.dx@gmail.com>
2018-05-22 20:50:33 +0200

    Wired up Moves History component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Chess from 'chess.js';
 
 import Chessboard from './components/Chessboard';
@@ -7,14 +7,23 @@ import MovesHistory from './components/MovesHistory';
 const chess = new Chess();
 const helperArray = Array.from({ length: 8 }, (v, k) => k + 1);
 
-const App = () => (
-  <div className="App">
-    <h1>ShallowBlue.js</h1>
-    <div className="rows">{helperArray.map(i => <p key={i}>{9 - i}</p>)}</div>
-    <Chessboard chess={chess} />
-    <MovesHistory chess={chess} />
-    <div className="cols">{helperArray.map(i => <p key={i}>{String.fromCharCode(i + 96)}</p>)}</div>
-  </div>
-);
-
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chess,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>ShallowBlue.js</h1>
+        <div className="rows">{helperArray.map(i => <p key={i}>{9 - i}</p>)}</div>
+        <Chessboard chess={this.state.chess} onMove={chess => this.setState({chess})}/>
+        <MovesHistory chess={this.state.chess} />
+        <div className="cols">{helperArray.map(i => <p key={i}>{String.fromCharCode(i + 96)}</p>)}</div>
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/MovesHistory.js
+++ src/components/MovesHistory.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MovesHistory = () => (
-  <div className="moves">
-    <h2>Moves</h2>
-    <hr />
-  </div>
-);
+class MovesHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chess: props.chess,
+    }
+  }
+  render() {
+    let history = this.state.chess.history({ verbose: true })
+    console.log(history.length);
+    return (
+      <div className="moves">
+        <h2>Moves</h2>
+        <hr />
+        <ul>
+          {history.map(record => (
+            <li key={record.san}>
+              <span>
+                <img
+                  alt="Chess Piece"
+                  draggable="false"
+                  src={`src/pieces/${record.piece}${record.color}.svg`}
+                  height="30px"
+                  width="30px"
+                />
+                {record.from} -> {record.to}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default MovesHistory;
