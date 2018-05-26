107a115854915665dd88a22ab47186ce80889b46
krokowski0x <krokowski.dx@gmail.com>
2018-05-26 10:08:06 +0200

    Applied linter rules where possible

--- src/utils/minimax.js
+++ src/utils/minimax.js
@@ -2,55 +2,52 @@ import { evaluateBoard } from './boardEvaluation';
 
 export let positionCount = 0;
 
-const minimaxRoot = function(depth, game, isMaximisingPlayer) {
+const minimaxRoot = (depth, game, isMaximisingPlayer) => {
 
-    let newGameMoves = game.moves();
-    let bestMove = -9999;
-    let bestMoveFound;
+  const newGameMoves = game.moves();
+  let bestMove = -9999;
+  let bestMoveFound;
 
-    for (let i = 0; i < newGameMoves.length; i++) {
-      let newGameMove = newGameMoves[i]
-      game.move(newGameMove);
-      let value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
-      game.undo();
-      if (value >= bestMove) {
-          bestMove = value;
-          bestMoveFound = newGameMove;
-      }
+  for (let i = 0; i < newGameMoves.length; i++) {
+    const newGameMove = newGameMoves[i];
+    game.move(newGameMove);
+    const value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
+    game.undo();
+    if (value >= bestMove) {
+      bestMove = value;
+      bestMoveFound = newGameMove;
     }
-    return bestMoveFound;
+  }
+  return bestMoveFound;
 };
 
-const minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
-    positionCount++;
-    if (depth === 0)
-      return -evaluateBoard(game.board());
+const minimax = (depth, game, alpha, beta, isMaximisingPlayer) => {
+  positionCount++;
+  if (depth === 0)
+    return -evaluateBoard(game.board());
 
-    let newGameMoves = game.moves();
+  const newGameMoves = game.moves();
+  let bestMove = -9999;
 
-    if (isMaximisingPlayer) {
-      let bestMove = -9999;
-      for (let i = 0; i < newGameMoves.length; i++) {
-        game.move(newGameMoves[i]);
-        bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
-        game.undo();
-        alpha = Math.max(alpha, bestMove);
-        if (beta <= alpha)
-          return bestMove;
-        }
-      return bestMove;
-    } else {
-      let bestMove = 9999;
-      for (let i = 0; i < newGameMoves.length; i++) {
-        game.move(newGameMoves[i]);
-        bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
-        game.undo();
-        beta = Math.min(beta, bestMove);
-        if (beta <= alpha)
-          return bestMove;
-        }
-      return bestMove;
+  if (isMaximisingPlayer)
+    for (let i = 0; i < newGameMoves.length; i++) {
+      game.move(newGameMoves[i]);
+      bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+      game.undo();
+      alpha = Math.max(alpha, bestMove);
+      if (beta <= alpha)
+        return bestMove;
     }
+  else
+    for (let i = 0; i < newGameMoves.length; i++) {
+      game.move(newGameMoves[i]);
+      bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+      game.undo();
+      beta = Math.min(beta, bestMove);
+      if (beta <= alpha)
+        return bestMove;
+    }
+  return bestMove;
 };
 
 export const calcBestMove = game => minimaxRoot(3, game, true);
