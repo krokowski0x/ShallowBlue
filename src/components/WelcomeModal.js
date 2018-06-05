bf441a186ffdd01a7ff503eba48b01ee71a5d8bc
krokowski0x <krokowski.dx@gmail.com>
2018-06-05 16:52:44 +0200

    Heroku deployment

--- src/components/WelcomeModal.js
+++ src/components/WelcomeModal.js
@@ -7,11 +7,11 @@ const options = [
   {
     name: 'Dumb', value: '1', class: 'easy', description: 'More like an Artifficial Dumbness type of difficulty.\n AI making random moves from available ones.',
   }, {
-    name: 'Weak', value: '2', class: 'moderate', description: 'AI using minimax game tree search with alfa-beta pruning improvement.\n Search depth: 2, evaluationg about 100 - 1k positions per move.',
+    name: 'Weak', value: '2', class: 'moderate', description: 'AI using minimax game tree search with alfa-beta pruning improvement.\n Search depth: 2, evaluating about 100 - 1k positions per move.',
   }, {
-    name: 'Fine', value: '3', class: 'moderate', description: 'AI using minimax game tree search with alfa-beta pruning improvement.\n Search depth: 3, evaluationg about 1k - 10k positions per move.',
+    name: 'Fine', value: '3', class: 'moderate', description: 'AI using minimax game tree search with alfa-beta pruning improvement.\n Search depth: 3, evaluating about 1k - 10k positions per move.',
   }, {
-    name: 'Tough', value: '4', class: 'moderate', description: 'AI using minimax game tree search with alfa-beta pruning improvement.\n Search depth: 4, evaluationg about 10k - 100k positions per move.',
+    name: 'Tough', value: '4', class: 'moderate', description: 'AI using minimax game tree search with alfa-beta pruning improvement.\n Search depth: 4, evaluating about 10k - 100k positions per move.',
   }, {
     name: 'Badass', value: '5', class: 'hard', description: 'Tensorflow.js trained Neural Network using Reinforcement Learning method.\n Work in progress.\n Available soon.',
   }];
